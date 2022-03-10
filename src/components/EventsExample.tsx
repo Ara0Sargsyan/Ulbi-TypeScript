3909125e8986199e98a36962f9f65b8c4bfe104b
Ara <arasargsyan567@gmail.com>
2022-03-09 22:49:23 -0800

    added 2 div in events

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -12,10 +12,24 @@ const [value, setValue] = useState<string>('')
         console.log(value);
     }
 
+    const draghandler = (e:React.DragEvent<HTMLDivElement>) => {
+        console.log("cl");
+    }
+
     return (
         <div>
             <input type="text" value={value} onChange={changehandler} />
             <button onClick={clickHandler} >button</button>
+            <div
+                style={{width: 200, height: 200, background: "red" }}
+                draggable
+                onDrag={draghandler}
+            >
+            </div>
+            <div
+                style={{width: 200, height: 200, background: "red", marginTop: "10px" }}
+            >
+            </div>
         </div>
     )
 }
